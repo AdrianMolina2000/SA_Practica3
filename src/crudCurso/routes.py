2c720b4664f4eb53a07a4a2956b833aea206a710
Diego <diego.fernando1399@gmail.com>
2023-02-21 23:23:45 -0600

    Tokens

--- src/crudCurso/routes.py
+++ src/crudCurso/routes.py
@@ -7,56 +7,79 @@ crudCurso = Blueprint('crudCurso', __name__,url_prefix='/crudCurso')
 @crudCurso.route('/setcurso', methods=['POST', 'GET', 'PUT', 'DELETE'])
 def setcurso():
     if request.method == 'POST':
+        token = request.headers.get('Token')
         try:
-            code_course = request.json['code_course']
-            name_course = request.json['name_course']
-            credit_course = request.json['credit_course']
-            pre_course = request.json['pre_course']
-            optional = request.json['optional']
+            tokenDatos = jwt.decode(token, "secret", algorithms=["HS256"])
+            tokenDatos.pop("exp",None)
+            try:
+                code_course = request.json['code_course']
+                name_course = request.json['name_course']
+                credit_course = request.json['credit_course']
+                pre_course = request.json['pre_course']
+                optional = request.json['optional']
 
-            if code_course <= 0:
-                return jsonify({'status': 400, 'descripcion': 'El codigo del curso no existe'})
-            if name_course == "":
-                return jsonify(({'status': 400, 'description': 'Nombre del curso no puede estar vacion'}))
-            if credit_course < 0:
-                return jsonify(({'status': 400, 'description': 'Creditos del curso no pueden ser negativos'}))
-            if len(pre_course) > 0:
-                for course in pre_course:
-                    if course <= 0:
-                        return jsonify({'status': 400, 'descripcion': f'El codigo {course} del pre-curso no existe'})
-            if optional < 0 or optional > 1:
-                return jsonify(({'status': 400, 'description': 'Valor de opcional no valido'}))
+                if code_course <= 0:
+                    return jsonify({'status': 400, 'descripcion': 'El codigo del curso no existe'})
+                if name_course == "":
+                    return jsonify(({'status': 400, 'description': 'Nombre del curso no puede estar vacion'}))
+                if credit_course < 0:
+                    return jsonify(({'status': 400, 'description': 'Creditos del curso no pueden ser negativos'}))
+                if len(pre_course) > 0:
+                    for course in pre_course:
+                        if course <= 0:
+                            return jsonify({'status': 400, 'descripcion': f'El codigo {course} del pre-curso no existe'})
+                if optional < 0 or optional > 1:
+                    return jsonify(({'status': 400, 'description': 'Valor de opcional no valido'}))
 
-                    
-            return jsonify({"status" : 200})
-        except:
+                        
+                return jsonify({"status" : 200})
+            except:
                 return jsonify({'status': 400, 'descripcion': 'Datos incorrectos para agregar curso'})
+        except jwt.ExpiredSignatureError:
+            return jsonify({"descripcion" : "JWT ya expiro", "status" : 400})
+        except jwt.InvalidTokenError:
+            return jsonify({"descripcion"  : "Error en token enviado", "status" : 400})
+
     else:
         return jsonify({'status': 500, 'descripcion': 'Metodo no manejado'})
 
 @crudCurso.route('/deleteCurso', methods=['POST', 'GET', 'PUT', 'DELETE'])
 def deletecurso():
     if request.methods == 'DELETE':
+        token = request.headers.get('Token')
         try:
-            code_course = request.json['code_course']
-            if code_course <= 0:
-                return jsonify(({'status': 400, 'descripcopn': 'Codigo del curso no existe'}))
-            return jsonify({'status': 200})
-        except:
-            return jsonify({'status': 400, 'descripcion': 'Datos incorrectos para eliminar curso'})
+            tokenDatos = jwt.decode(token, "secret", algorithms=["HS256"])
+            tokenDatos.pop("exp",None)
+            try:
+                code_course = request.json['code_course']
+                if code_course <= 0:
+                    return jsonify(({'status': 400, 'descripcopn': 'Codigo del curso no existe'}))
+                return jsonify({'status': 200})
+            except:
+                return jsonify({'status': 400, 'descripcion': 'Datos incorrectos para eliminar curso'})
+        except jwt.ExpiredSignatureError:
+            return jsonify({"descripcion" : "JWT ya expiro", "status" : 400})
+        except jwt.InvalidTokenError:
+            return jsonify({"descripcion"  : "Error en token enviado", "status" : 400})
     else:
         return jsonify({'status': 500, 'descripcion': 'Metodo no manejado'})
 
 @crudCurso.route('/getAllCursos', methods=['POST', 'GET', 'PUT', 'DELETE'])
 def getallcursos():
     if request.method == 'GET':
+        token = request.headers.get('Token')
         try:
-            return jsonify({
-                'status': 200,
-                'courses': [
-                    {"code_course": 780, "name_course": "Software Avanzado", "credit_course": 8, "pre_course": [785], "optiona": 1}
-                ]})
-        except:
-            return jsonify({'status': 400, 'descripcion': 'Datos incorrectos para obtener los cursos'})
+            try:
+                return jsonify({
+                    'status': 200,
+                    'courses': [
+                        {"code_course": 780, "name_course": "Software Avanzado", "credit_course": 8, "pre_course": [785], "optiona": 1}
+                    ]})
+            except:
+                return jsonify({'status': 400, 'descripcion': 'Datos incorrectos para obtener los cursos'})
+        except jwt.ExpiredSignatureError:
+            return jsonify({"descripcion" : "JWT ya expiro", "status" : 400})
+        except jwt.InvalidTokenError:
+            return jsonify({"descripcion"  : "Error en token enviado", "status" : 400})
     else:
         return jsonify({'status': 500, 'descripcion': 'Metodo no manejado'})
--- src/horariosSemestre/routes.py
+++ src/horariosSemestre/routes.py
@@ -7,63 +7,92 @@ horariosSemestre = Blueprint('horariosSemestre', __name__,url_prefix='/horariosS
 @horariosSemestre.route('/addHorarioCurso', methods=['POST', 'GET', 'PUT', 'DELETE'])
 def addhorariocurso():
     if request.method == 'POST':
+        token = request.headers.get('Token')
         try:
-            code_course = request.json['code_course']
-            code_doce = request.json['code_doce']
-            section = request.json['section']
-            hour_init = request.json['hour_init']
-            hour_finish = request.json['hour_finish']
-            dates = request.json['dates']
+            tokenDatos = jwt.decode(token, "secret", algorithms=["HS256"])
+            tokenDatos.pop("exp",None)
+            try:
+                code_course = request.json['code_course']
+                code_doce = request.json['code_doce']
+                section = request.json['section']
+                hour_init = request.json['hour_init']
+                hour_finish = request.json['hour_finish']
+                dates = request.json['dates']
 
-            if code_course <= 0:
-                return jsonify({'status': 400, 'descripcion': 'El codigo del curso no existe'})
-            if code_doce <= 0:
-                return jsonify({'status': 400, 'descripcion': 'El codigo del docente no existe'})
-            if section == "":
-                return jsonify({'status': 400, 'descripcion': 'Seccion no puede estar vacio'})
-            if hour_init == "":
-                return jsonify({'status': 400, 'descripcion': 'Hora de inicio no puede estar vacio'})
-            if hour_finish == "":
-                return jsonify({'status': 400, 'descripcion': 'Hora de finalizar no puede estar vacio'})
-            if len(dates) <= 0:
-                return jsonify({'status': 400, 'descripcion': 'Lista de dias no puede estar vacio'})
-            for day in dates:
-                if day != "Lunes" and day != "Martes" and day != "Miercoles" and day != "Jueves" and day != "Viernes" and day != "Sabado" and day != "Domingo":
-                    return jsonify(({'status': 400, 'descripcion': f'Dia {day} no valido'}))
-            return jsonify({"status" : 200})
-        except:
-            return jsonify({'status': 400, 'descripcion': 'Datos incorrectos para agregar horario de curso'})
+                if code_course <= 0:
+                    return jsonify({'status': 400, 'descripcion': 'El codigo del curso no existe'})
+                if code_doce <= 0:
+                    return jsonify({'status': 400, 'descripcion': 'El codigo del docente no existe'})
+                if section == "":
+                    return jsonify({'status': 400, 'descripcion': 'Seccion no puede estar vacio'})
+                if hour_init == "":
+                    return jsonify({'status': 400, 'descripcion': 'Hora de inicio no puede estar vacio'})
+                if hour_finish == "":
+                    return jsonify({'status': 400, 'descripcion': 'Hora de finalizar no puede estar vacio'})
+                if len(dates) <= 0:
+                    return jsonify({'status': 400, 'descripcion': 'Lista de dias no puede estar vacio'})
+                for day in dates:
+                    if day != "Lunes" and day != "Martes" and day != "Miercoles" and day != "Jueves" and day != "Viernes" and day != "Sabado" and day != "Domingo":
+                        return jsonify(({'status': 400, 'descripcion': f'Dia {day} no valido'}))
+                return jsonify({"status" : 200})
+            except:
+                return jsonify({'status': 400, 'descripcion': 'Datos incorrectos para agregar horario de curso'})
+        except jwt.ExpiredSignatureError:
+            return jsonify({"descripcion" : "JWT ya expiro", "status" : 400})
+        except jwt.InvalidTokenError:
+            return jsonify({"descripcion"  : "Error en token enviado", "status" : 400})
     else:
         return jsonify({'status': 500, 'descripcion': 'Metodo no manejado'})
 
 @horariosSemestre.route('/removeHorarioCurso', methods=['POST', 'GET', 'PUT', 'DELETE'])
 def removehorario():
     if request.method == 'DELETE':
+        token = request.headers.get('Token')
         try:
-            code_course = request.json['code_course']
-            if code_course <= 0:
-                return jsonify({'status': 400, 'descripcion': 'El codigo del curso no existe'})
-            return jsonify({"status" : 200})
-        except:
-                return jsonify({'status': 400, 'descripcion': 'Datos incorrectos para eliminar horario del curso'})
+            tokenDatos = jwt.decode(token, "secret", algorithms=["HS256"])
+            tokenDatos.pop("exp",None)
+            try:
+                code_course = request.json['code_course']
+                section = request.json['section']
+                if code_course <= 0:
+                    return jsonify({'status': 400, 'descripcion': 'El codigo del curso no existe'})
+                if section == "":
+                    return jsonify({'status': 400, 'descripcion': 'La seccion no debe estar vacia'})
+                return jsonify({"status" : 200})
+            except:
+                 return jsonify({'status': 400, 'descripcion': 'Datos incorrectos para eliminar horario del curso'})
+            
+        except jwt.ExpiredSignatureError:
+            return jsonify({"descripcion" : "JWT ya expiro", "status" : 400})
+        except jwt.InvalidTokenError:
+            return jsonify({"descripcion"  : "Error en token enviado", "status" : 400})
     else:
         return jsonify({'status': 500, 'descripcion': 'Metodo no manejado'})
 
 @horariosSemestre.route('/getHorariosCursos', methods=['POST', 'GET',  'PUT', 'DELETE'])
 def gethorario():
     if request.method == 'GET':
+        token = request.headers.get('Token')
         try:
-            return ({'status': 200, 'course': [
-                {
-                     "code_course" : 780,
-                     "name_course" : "Software Avanzado",
-                     "credit_course" : 8,
-                     "pre_course" : [ 785 ],
-                     "optional" : 1
-                   }
+            tokenDatos = jwt.decode(token, "secret", algorithms=["HS256"])
+            tokenDatos.pop("exp",None)
+            try:
+                
+                return ({'status': 200, 'course': [
+                    {
+                        "code_course" : 780,
+                        "name_course" : "Software Avanzado",
+                        "credit_course" : 8,
+                        "pre_course" : [ 785 ],
+                        "optional" : 1
+                    }
 
-            ]})
-        except:
-            return jsonify({'status': 400, 'descripcion': 'Datos incorrectos para obtener los cursos'})
+                ]})
+            except:
+                return jsonify({'status': 400, 'descripcion': 'Datos incorrectos para obtener los cursos'})
+        except jwt.ExpiredSignatureError:
+            return jsonify({"descripcion" : "JWT ya expiro", "status" : 400})
+        except jwt.InvalidTokenError:
+            return jsonify({"descripcion"  : "Error en token enviado", "status" : 400})
     else:
         return jsonify({'status': 500, 'descripcion': 'Metodo no manejado'})
